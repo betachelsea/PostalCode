2d98135d5f816fc9a83cd8cf07faeb17b22e84f1
beta_chelsea <beta.chelsea@gmail.com>
2014-07-15 08:46:58 +0900

    正常に取得を確認できる状態に修正

--- main.js
+++ main.js
@@ -1,25 +1,10 @@
 $(function() {
 
+    $.getJSON("http://api.thni.net/jzip/X0401/JSONP/J/state_index.js?callback=?");
 
-    function StateIndex(data) {
-        console.log(data);
-    }
-    $.ajax({
-        type: "GET",
-        dataType: "jsonp",
-        url: "http://api.thni.net/jzip/X0401/JSONP/J/state_index.js",
-        jsonp: 'StateIndex',
-        success: function(data, dt, jqxhr) {
-            console.log("success");
-        },
-        timeout: 30000,
-        error: function(e) {
-            console.error(e);
-            alert("エラーでました");
-        }
-    });
-
-    function jsoncallback() {
-        alert("jsoncallback");
-    }
 });
+
+function StateIndex(data) {
+    console.log(data);
+};
+
